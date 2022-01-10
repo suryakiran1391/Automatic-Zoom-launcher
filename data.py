4929cb4a6f435ef01758fbebd31e556081a12b39
suryakiran1391 <68011483+suryakiran1391@users.noreply.github.com>
2022-01-10 20:25:10 +0530

    Create data.py

---
+++ data.py
@@ -0,0 +1,18 @@
+import webbrowser
+
+
+timetable = [
+    ["Name of the first period", "start time", "end time"],
+    ["Name of the second period", "start time", "end time"],
+    ["Name of the third period", "start time", "end time"],
+    ["Name of the fifth period", "Start time", "end time"],
+    ["Name of the sixth period", "Start time    ", "end time"], 
+    ["Name of  Nth period", "Start time", "end time"]
+    
+]
+#make sure the name of the Periods in time table match with the function name
+def subject1():
+     webbrowser.open('subject1 zoomlink')
+def subject2():
+    webbrowser.open('subject2 zoomlink')
+#extend the number of functions according to the number of Subjets you have to attend
